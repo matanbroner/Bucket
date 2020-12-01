6616da5f721de9d76b3ef02550a6b0c20803dd02
kchettia <kchettia@ucsc.edu>
2020-12-01 15:41:07 -0800

    Basic causal test for single node with repl factor of 2

--- scripts/test_causal.py
+++ scripts/test_causal.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 import unittest 
 import requests
 import json
@@ -26,36 +27,40 @@ class causal_test(unittest.TestCase):
        return requests.get ('http://localhost:%s/kvs/keys/%s'%(port,key),
                             json = {'causal-context':context},
                             headers = {"Content-Type": "application/json"}) 
-                            
+    
     def test_causal_1(self):
+        
         # Write X to R1
         response = self.put_request("13801","x","1","")
         contents = response.json()
-        self.assertEqual(contents["message"],add_resp)
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(contents["message"],update_resp)
+        self.assertEqual(response.status_code,200)
+   
         # Read X from R2 with causal-context from prev write. Should Result in error
         response= self.get_request("13802","x",contents["causal-context"])
         contents2 = response.json()
-        self.assertEqual(contents2["message"],put_error_msg)
+        self.assertEqual(contents2["message"],get_error_msg)
         self.assertEqual(contents2["error"],unable_error_resp)
         self.assertEqual(response.status_code,400)
+  
+     
 
     def test_causal_2(self):
         # Write X to R1
-        response = self.put_request("13801","x","1","")
+        response = self.put_request("13801","y","1","")
         contents = response.json()
-        self.assertEqual(contents["message"],add_resp)
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(contents["message"],update_resp)
+        self.assertEqual(response.status_code,200)
+
         # Read X from R2 without causal-context. Should Result in key DNE
-        response= self.get_request("13802","x","")
+        response= self.get_request("13802","y","")
         contents2 = response.json()
         self.assertEqual(contents2["message"],get_error_msg)
-        self.assertEqual(contents2["error"],get_error_)
+        self.assertEqual(contents2["error"],get_error_resp)
         self.assertEqual(contents2["doesExist"],False)
-        self.assertEqual(response.status_code,400)
+        self.assertEqual(response.status_code,404)
 
       
-
 
 if __name__ == '__main__':
     unittest.main()
