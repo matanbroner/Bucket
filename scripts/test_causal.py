a1f4e3dff5b8e96f4d4f8392afe7a4f39445f347
kchettia <kchettia@ucsc.edu>
2020-12-08 19:50:45 -0800

    Added Test2 to test_causal

--- scripts/test_causal.py
+++ scripts/test_causal.py
@@ -33,6 +33,7 @@ class causal_test(unittest.TestCase):
         )
 
     def test_causal_1(self):
+        print ("\nTEST 1")
         # C1 Writes X to R1
         response = self.put_request("13801", "x", "1", "")
         contents = response.json()
@@ -72,6 +73,57 @@ class causal_test(unittest.TestCase):
         response = self.get_request("13801", "y", c2)
         self.assertEqual(response.status_code, 400)
 
+
+
+    '''
+    C1      R1     C2
+    |  a =1 |      |
+    |-----> |  R(a)|
+    |       |<-----|
+    |  b=1  |      |
+    |-----> |      |
+    |  c=1  |      |
+    |-----> |  R(c)|
+    |       |<-----|
+
+    '''
+    def test_causal_2(self):
+        # C1 Writes a to R1
+        print ("\n\nTEST 2")
+        response = self.put_request("13801", "a", "1", "")
+        contents = response.json()
+        c1 = contents["causal-context"]
+        print("C1 context after write a=1 to R1:", c1)
+        self.assertTrue(200 <= response.status_code <= 201)
+
+        #C2 Reads a from R1
+        response = self.get_request("13801", "a", "")
+        contents = response.json()
+        c2 = contents["causal-context"]
+        print("C2 context after reads a from R1:", c2)
+        print("Y R2 value:", contents["value"])
+        self.assertEqual(response.status_code, 200)
+
+        # C1 Writes b to R1
+        response = self.put_request("13801", "b", "1", c1)
+        contents = response.json()
+        c1 = contents["causal-context"]
+        print("C1 context after write b=1 to R1:", c1)
+        self.assertTrue(200 <= response.status_code <= 201)
+        
+        # C1 Writes c to R1
+        response = self.put_request("13801", "c", "1", c1)
+        contents = response.json()
+        c1 = contents["causal-context"]
+        print("C1 context after write c=1 to R1:", c1)
+        self.assertTrue(200 <= response.status_code <= 201)
+
+        #C2 Reads c from R1
+        response = self.get_request("13801", "c", c2)
+        contents = response.json()
+        c2 = contents["causal-context"]
+        print("C2 context after read c from R1:", c2)
+        self.assertEqual(response.status_code, 503)
 
 if __name__ == "__main__":
     unittest.main()
