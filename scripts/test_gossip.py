0a18921e4618a398fc7d41fc95449b89e8288b1c
kchettia <kchettia@ucsc.edu>
2020-12-09 22:40:25 -0800

    Fixed Test 2

--- scripts/test_gossip.py
+++ scripts/test_gossip.py
@@ -37,8 +37,18 @@ class gossip_test(unittest.TestCase):
             json={"causal-context": context},
             headers={"Content-Type": "application/json"},
         )
+    def view_change(self, port):
+        return requests.put(
+            "http://localhost:%s/kvs/view-change" % (port),
+            json={"view":"10.10.0.2:13800,10.10.0.3:13800,10.10.0.4:13800,10.10.0.5:13800","repl-factor":2},
+            headers={"Content-Type": "application/json"},
+        )
 
     def test_gossip_1(self):
+        response = self.view_change("13801")
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+
         response = self.put_request("13801", "x", "0", "{}")
         contents = response.json()
         c1 = contents["causal-context"]
@@ -53,7 +63,7 @@ class gossip_test(unittest.TestCase):
         contents = response.json()
         self.assertEqual(response.status_code, 400)
 
-        time.sleep(3)
+        time.sleep(5)
         response = self.get_request("13804",  "y", c1)
         contents=response.json()
         c1 = contents["causal-context"]
@@ -62,70 +72,60 @@ class gossip_test(unittest.TestCase):
         self.assertTrue(200 <= response.status_code <= 201)
 
     def test_gossip_2(self):
-        response = self.put_request("13801", "x1", "0", "{}")
+        response = self.put_request("13801", "y1", "0", "")
         self.assertTrue(200 <= response.status_code <= 201)
         c1= response.json()["causal-context"]
 
-        response = self.put_request("13801", "y1", "1", "{}")
+        response = self.put_request("13804", "x1", "0", "")
         self.assertTrue(200 <= response.status_code <= 201)
         c2= response.json()["causal-context"]
 
-        response = self.put_request("13804", "test_key1", "2", "")
-        self.assertTrue(200 <= response.status_code <= 201)
-        c3= response.json()["causal-context"]
-  
-        response = self.get_request("13803",  "x1", c1)
-        contents=response.json()
-        self.assertEqual(contents["message"], get_resp)
-        self.assertEqual(contents["doesExist"], True)
-        self.assertTrue(200 <= response.status_code <= 201)
-
-        #Might have an error here. Need to check logic
-        response = self.get_request("13804",  "y1", c2)
-        contents=response.json()
-        self.assertEqual(contents["message"], get_error_msg)
-        self.assertEqual(response.status_code, 404)
-
-        response = self.get_request("13801",  "test_key1", c3)
-        contents=response.json()
-        self.assertEqual(contents["message"], get_resp)
-        self.assertEqual(contents["doesExist"], True)
-        self.assertTrue(200 <= response.status_code <= 201)
-
-
-    def ttest_gossip_3(self):
-        response = self.put_request("13801", "x1", "1", "{}")
+        time.sleep(5)
+        response = self.put_request("13803", "y1", "1",c1)
         self.assertTrue(200 <= response.status_code <= 201)
         c1= response.json()["causal-context"]
-        response = self.put_request("13801", "y1", "2", "{}")
+
+        response = self.put_request("13802", "x1", "1", c2)
         self.assertTrue(200 <= response.status_code <= 201)
         c2= response.json()["causal-context"]
-        response = self.put_request("13804", "test_key1", "3", "")
-        self.assertTrue(200 <= response.status_code <= 201)
-        c3= response.json()["causal-context"]
-        time.sleep(3)
-        
-        response = self.get_request("13803",  "x1", c1)
-        contents=response.json()
-    
-        self.assertEqual(contents["message"], get_resp)
-        self.assertEqual(contents["doesExist"], True)
-        self.assertTrue(200 <= response.status_code <= 201)
-        self.assertEqual(contents["value"],"1")
-        response = self.get_request("13804",  "y1", c2)
-        contents=response.json()
-        
-        self.assertEqual(contents["message"], get_resp)
-        self.assertEqual(contents["doesExist"], True)
-        self.assertTrue(200 <= response.status_code <= 201)
-        self.assertEqual(contents["value"],"2")
+ 
+        response = self.get_request("13804", "y1", c2)
+        contents = response.json()
+        self.assertEqual(response.status_code, 400)#400
 
-        response = self.get_request("13801",  "test_key1", c3)
-        contents=response.json()
-        self.assertEqual(contents["message"], get_resp)
-        self.assertEqual(contents["doesExist"], True)
-        self.assertTrue(200 <= response.status_code <= 201)
-        self.assertEqual(contents["value"],"3")
+        response = self.get_request("13801", "x1", c1)
+        contents = response.json()
+        self.assertEqual(response.status_code, 400)#400
+
+        response = self.get_request("13804", "y1", "")
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(contents["value"], "0")
+
+        response = self.get_request("13801", "x1", "")
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(contents["value"], "0")
+
+        time.sleep(5)
+        response = self.get_request("13804", "y1", c1)
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(contents["value"], "1")
+
+        response = self.get_request("13801", "x1", c2)
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(contents["value"], "1")
+
+        response = self.del_request("13804", "y1", c1)
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+
+        response = self.del_request("13801", "x1", c2)
+        contents = response.json()
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
