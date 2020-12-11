b0ceb810143a22599d3afa701860e179ff131c73
kchettia <kchettia@ucsc.edu>
2020-12-11 03:37:53 -0800

    Added test 2

--- scripts/test_causal2.py
+++ scripts/test_causal2.py
@@ -42,7 +42,6 @@ class gossip_test(unittest.TestCase):
 
     # 13801,13803 in 1 View 13802,13804 in another view
     def test_causal_1(self):
-
         response = self.put_request("13801", "x", "0", "{}")
         contents = response.json()
         c1 = contents["causal-context"]
@@ -51,7 +50,7 @@ class gossip_test(unittest.TestCase):
         response = self.put_request("13802", "y", "0", c1)
         contents = response.json()
         c1 = contents["causal-context"]
-        self.assertEqual(response.status_code, 200)
+        self.assertTrue(200 <= response.status_code <= 201)
 
         response = self.get_request("13802", "x", c1)
         contents = response.json()
@@ -69,10 +68,34 @@ class gossip_test(unittest.TestCase):
         contents = response.json()
         self.assertEqual(response.status_code, 404)
         
-       
+    def test_causal_2(self):
+        response = self.put_request("13801", "x", "0", "{}")
+        contents = response.json()
+        c1 = contents["causal-context"]
+        self.assertTrue(200 <= response.status_code <= 201)
         
+        response = self.put_request("13802", "y", "0", c1)
+        contents = response.json()
+        c2 = contents["causal-context"]
+        self.assertTrue(200 <= response.status_code <= 201)
 
+        response = self.del_request("13801", "x", c1)
+        contents = response.json()
+        c1 = contents["causal-context"]
+        self.assertEqual(response.status_code, 200)
 
+        response = self.del_request("13802", "y", c2)
+        contents = response.json()
+        c2 = contents["causal-context"]
+        self.assertEqual(response.status_code, 200)
+
+        response = self.get_request("13801", "x", c2)
+        contents = response.json()
+        self.assertEqual(response.status_code, 400)
+
+        response = self.get_request("13802", "x", c1)
+        contents = response.json()
+        self.assertEqual(response.status_code, 400)
 
 if __name__ == "__main__":
     unittest.main()
